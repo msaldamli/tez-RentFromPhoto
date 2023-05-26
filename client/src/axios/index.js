756ce5bbe5b675572af3a767a079cb5a86f67fab
Muhammed said saldamlı <msaldamli48@gmail.com>
2023-05-26 19:31:25 +0300

    api commit

--- client/src/axios/index.js
+++ client/src/axios/index.js
@@ -3,7 +3,8 @@
 import axios from 'axios';
 const api = axios.create({
   // baseURL: 'https://tez2-api.onrender.com',http://localhost:3500
-  baseURL: 'http://localhost:3500',
+  // baseURL: 'http://localhost:3500',
+  baseURL: 'https://tez-rent-from-photo.vercel.app',
 });
 
 export default api.defaults.baseURL;
