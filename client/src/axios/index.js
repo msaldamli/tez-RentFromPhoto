a49c75bcac20fda8ff04424e3948f10a1ac10329
Muhammed said saldamlı <msaldamli48@gmail.com>
2023-05-30 07:12:08 +0300

    api

--- client/src/axios/index.js
+++ client/src/axios/index.js
@@ -3,8 +3,8 @@
 import axios from 'axios';
 const api = axios.create({
   // baseURL: 'https://tez2-api.onrender.com',http://localhost:3500
-  baseURL: 'http://localhost:3500',
-  // baseURL: 'https://rentfromfoto-api.onrender.com',
+  // baseURL: 'http://localhost:3500',
+  baseURL: 'https://rentfromfoto-api.onrender.com',
 });
 
 export default api.defaults.baseURL;
